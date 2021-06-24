6d888678b437a31ca56719b34da952ac8b6b84e3
alexe-mgn <alexe-mgn@mail.ru>
2021-06-24 21:39:14 +0300

    switched date_utils to built-in timezone.

--- lib/date_utils.py
+++ lib/date_utils.py
@@ -13,22 +13,7 @@ _month_pref = [e.lower()[:-1] for e in calendar.month_name[1:]]
 locale.setlocale(locale.LC_TIME, old_loc)
 
 
-class Timezone(datetime.tzinfo):
-    """
-    Timezone determined by utc offset.
-    """
-
-    def __init__(self, utc_offset: int = 0):
-        self._utc_offset = utc_offset
-
-    def utcoffset(self, dt: Optional[datetime.datetime]) -> Optional[datetime.timedelta]:
-        return datetime.timedelta(hours=self._utc_offset)
-
-    def dst(self, dt: Optional[datetime.datetime]) -> Optional[datetime.timedelta]:
-        return datetime.timedelta()
-
-
-MCHS_TZ = Timezone(3)
+MCHS_TZ = datetime.timezone(datetime.timedelta(hours=3), name="MSK")
 
 
 def month_to_int(name: str) -> int:
